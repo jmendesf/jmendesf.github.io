a8f4051e237aa0f294095b0f610296ad6c34e35f
jmendesf <jmendesforte@protonmail.com>
2025-10-26 17:07:30 +0000

    Deploying to gh-pages from @ jmendesf/jmendesf.github.io@ed166bbfd19e95d5168e2e909b7f2027b3e589aa 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-repositories",
           title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          description: "You will find below some of the repositories I am (or was) working on, as well as the library Higra which I frequently use.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/repositories/";
